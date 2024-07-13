6271355db2b8c8309b199471cf6d42799eae06f4
Your Name <you@example.com>
2024-07-13 15:30:03 +0900

    test

--- qmlsink/main.cpp
+++ qmlsink/main.cpp
@@ -2,31 +2,8 @@
 #include <QQmlApplicationEngine>
 #include <QQuickItem>
 #include <QQuickWindow>
-#include <QRunnable>
 #include <gst/gst.h>
-
-class SetPlaying : public QRunnable {
-public:
-  SetPlaying(GstElement *);
-  ~SetPlaying();
-
-  void run();
-
-private:
-  GstElement *pipeline_;
-};
-
-SetPlaying::SetPlaying(GstElement *pipeline) { this->pipeline_ = pipeline ? static_cast<GstElement *>(gst_object_ref(pipeline)) : NULL; }
-
-SetPlaying::~SetPlaying() {
-  if (this->pipeline_)
-    gst_object_unref(this->pipeline_);
-}
-
-void SetPlaying::run() {
-  if (this->pipeline_)
-    gst_element_set_state(this->pipeline_, GST_STATE_PLAYING);
-}
+#include "SetPlaying.h"
 
 int main(int argc, char *argv[]) {
   int ret;
@@ -39,8 +16,6 @@ int main(int argc, char *argv[]) {
     GstElement *pipeline = gst_pipeline_new(NULL);
     GstElement *src      = gst_element_factory_make("videotestsrc", NULL);
     GstElement *glupload = gst_element_factory_make("glupload", NULL);
-    /* the plugin must be loaded before loading the qml file to register the
-     * GstGLVideoItem qml item */
     GstElement *sink = gst_element_factory_make("qmlglsink", NULL);
 
     g_assert(src && glupload && sink);
